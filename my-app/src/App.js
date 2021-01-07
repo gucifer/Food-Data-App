97f1018fc776a974974deb3e977e0ef6ffcdbdec
gucifer <f2016082@pilani.bits-pilani.ac.in>
2021-01-07 19:11:27 +0530

    SimpleSearch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+
+
+class FoodSearch extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {value: '', searched: false};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange = event => {
+    this.setState({value: event.target.value});
+  }
+  afterSubmission = event => {
+    this.setState({searched: true});
+    event.preventDefault();
+    this.componentDidMount();
+  }
+  componentDidMount() {
+    fetch("https://api.edamam.com/search?q=chicken&app_id=5b888f44&app_key=3dc654182b583fe360491aceb99db768")
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+  render() {
+    console.log(this.state.totalReactPackages);
+    return (
+      <div>
+      <form id="mainsearchform" onSubmit={this.afterSubmission}>
+        <input type="text" id="mainsearchfield" onChange={this.handleChange}/>
+        <input type="submit" value="Search" id="mainsearchbutton"/>
+      </form>
+      <p>{this.state.totalReactPackages}</p>
+      </div>
+    );
+  }
+
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FoodSearch name="Food item" />
   );
 }
 
