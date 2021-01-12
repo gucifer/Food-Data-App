710c6c97a8bc24738b28f59dc97a4252837f501e
gucifer <f2016082@pilani.bits-pilani.ac.in>
2021-01-12 19:45:44 +0530

    Adding Spoonacular changes

--- sponacular_api/app.py
+++ sponacular_api/app.py
@@ -2,6 +2,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return 'Hello World'
+
+
+@app.route('/api/search')
+def search_recipe():
+    req = request.get_json()
+    responce = dict()
+    responce = req
+    return jsonify(responce)
