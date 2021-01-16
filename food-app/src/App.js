07fa974fb1d23df2d75bdf274985a2b22aaac877
gucifer <f2016082@pilani.bits-pilani.ac.in>
2021-01-16 11:59:49 +0530

    Added React Components

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Searchbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'Search'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.checkKey = this.checkKey.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.props.parentCallback(this.state.value);
+    event.preventDefault();
+  }
+
+  checkKey(event){
+    if(event.key === 'Enter'){
+      this.props.parentCallback(this.state.value);
+    }
+  }
+  render() {
+    return (
+    <div id="searchbar">
+      <form onSubmit={e => { e.preventDefault(); }}>
+        <input type="text" placeholder={this.state.value} autofocus id="searchinput" onChange={this.handleChange} onKeyPress={this.checkKey}/>
+        <input type="button" value="Search" id="searchbutton" onClick={this.handleSubmit}/>
+      </form>
+    </div>  
+    )
+  }
+}
+
+class FoodList extends React.Component {
+
+  render() {
+    console.log('here');
+    return(
+      <div id="foodlist">{this.props.searchterm}</div>
+    );
+  }
+}
+
+class Mainpage extends React.Component {
+  state = { value: "" }
+  callbackFunction = (search) => {
+    this.setState({value: search});
+  }
+
+  render() {
+    return (
+      <div>
+        <Searchbar parentCallback = {this.callbackFunction}/>
+        <FoodList searchterm = {this.state.value}/>
+      </div>
+    );
+  }
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Mainpage/>
   );
 }
 
